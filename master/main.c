c348ee3d28055c62fb0480e9d8d7747c963bda25
pratamaizzat <pratama.izzat231298@gmail.com>
2019-11-28 20:57:25 +0700

    menabahkan source file main.c pada master dan slave.

---
+++ master/main.c
@@ -0,0 +1,15 @@
+/*
+ * main.c
+ *
+ *  Created on: Nov 28, 2019
+ *      Author: izzat
+ */
+#include <avr/io.h>
+#include <stdio.h>
+
+int main(void)
+{
+
+}
+
+
---
+++ slave/main.c
@@ -0,0 +1,14 @@
+/*
+ * main.c
+ *
+ *  Created on: Nov 28, 2019
+ *      Author: izzat
+ */
+#include <avr/io.h>
+#include <stdio.h>
+
+int main(void)
+{
+
+}
+
