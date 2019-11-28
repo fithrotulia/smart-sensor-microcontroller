96ee0b259a3e511f675ffa9d39896e84cfc98a86
pratamaizzat <pratama.izzat231298@gmail.com>
2019-11-28 22:30:36 +0700

    nenambahkan fitur UART.

--- master/main.c
+++ master/main.c
@@ -6,10 +6,32 @@
  */
 #include <avr/io.h>
 #include <stdio.h>
+#include <util/delay.h>
+
+#define F_CPU 16000000UL
+
+unsigned char num_variable;
+void USART_Init(void);
 
 int main(void)
 {
+	USART_Init();
+	num_variable=100;
+	while(1){
+		while(!(UCSR0A&(1<<UDRE0))); //wait until UDRE register empty
+		UDR0 = num_variable;
+		if(++num_variable>250){
+			num_variable=0;
+		}
+		_delay_ms(50);
+	}
 
 }
 
+void USART_Init(void)
+{
+	UCSR0C = (2<<UPM00)|(3<<UCSZ00);
+	UBRR0L =103;
+	UCSR0B = (1<<TXEN0);
+}
 
