bd3e77fc35736de7722c6de0d4afc61bbb84bd6e
pratamaizzat <pratama.izzat231298@gmail.com>
2019-11-29 09:43:44 +0700

    menambahkaan MyUSART.c dan MyUSART.h pada master.

--- master/main.c
+++ master/main.c
@@ -5,33 +5,24 @@
  *      Author: izzat
  */
 #include <avr/io.h>
-#include <stdio.h>
 #include <util/delay.h>
+#include "MyUSART.h"
 
-#define F_CPU 16000000UL
-
-unsigned char num_variable;
-void USART_Init(void);
+unsigned int num_variable;
 
 int main(void)
 {
 	USART_Init();
 	num_variable=100;
 	while(1){
-		while(!(UCSR0A&(1<<UDRE0))); //wait until UDRE register empty
-		UDR0 = num_variable;
+		USART_SendNum(num_variable);
+		USART_SendStr("\n\r");
 		if(++num_variable>250){
 			num_variable=0;
 		}
+
 		_delay_ms(50);
 	}
-
 }
 
-void USART_Init(void)
-{
-	UCSR0C = (2<<UPM00)|(3<<UCSZ00);
-	UBRR0L =103;
-	UCSR0B = (1<<TXEN0);
-}
 
