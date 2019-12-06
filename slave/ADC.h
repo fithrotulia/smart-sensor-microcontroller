58758f3b573ea6398cf828538f4318e359a7bc96
pratamaizzat <pratama.izzat231298@gmail.com>
2019-12-06 10:19:29 +0700

    menambahkan calibrasi sensor 2 dan uart file.

--- slave/ADC.h
+++ slave/ADC.h
@@ -14,6 +14,8 @@ void ADC_INISIALISASI(void);
 void READ_ADC(uint8_t channel);
 uint8_t HasilADCH(uint8_t channel);
 uint8_t HasilADCL(uint8_t channel);
+uint16_t TenADC(uint16_t channel);
+int ADCSingleRead(uint8_t adcpin);
 
 
 #endif /* ADC_H_ */
